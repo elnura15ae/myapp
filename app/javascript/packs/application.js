fe1581c68befc0f99740226651a8433dab87856a
elnura15ae <elnura15ae@gmail.com>
2021-04-23 14:55:18 +0200

    updates

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,10 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import Webpack from "webpack-dev-server"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+Webpack.start()
+
